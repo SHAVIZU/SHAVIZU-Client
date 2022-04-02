582f5262bf768a2963b514c321c8c3727571a307
LeeJinWoo <wlsdn5793@gmail.com>
2022-04-02 21:23:28 +0900

    ⚡️ :: change password publishing

---
+++ src/components/auth/changePw/ChangePw.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import * as S from "../styles";
+const ChangePw = () => {
+  return (
+    <S.Container>
+      <S.Title>비밀번호 변경</S.Title>
+      <S.Input placeholder="아이디" />
+      <S.Input placeholder="사업자 등록번호" />
+      <S.Input placeholder="변경할 비밀번호" type="password" />
+      <S.Input placeholder="비밀번호 확인" type="password" />
+      <S.Submit>변경</S.Submit>
+    </S.Container>
+  );
+};
+
+export default ChangePw;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { LoginPage } from "../pages";
+import { LoginPage, ChangePwPage } from "../pages";
 const Router: FC = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/change-pw" element={<ChangePwPage />} />
       </Routes>
     </BrowserRouter>
   );
