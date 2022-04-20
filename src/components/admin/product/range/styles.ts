fa40f4d1b8e3192627efb5fb3b6c892d18508486
LeeJinWoo <wlsdn5793@gmail.com>
2022-04-20 15:02:52 +0900

    ⚡️ :: 새로운 제품등록 range 추가

--- src/components/admin/product/range/styles.ts
+++ src/components/admin/product/range/styles.ts
@@ -4,6 +4,19 @@ export const Container = styled.div`
     width: 500px;
     height: 50px;
     display: flex;
-    justify-content: center;
-    align-items: flex-end;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 20px;
 `
+export const SettingSize = styled(Container)`
+    width: 50px;
+    height: 30px;
+    justify-content: center;
+    border: 1px solid black;
+    border-radius: 4px;
+    margin: 0;
+    & span {
+        color: black;
+        height: 30px;
+    }
+`;
