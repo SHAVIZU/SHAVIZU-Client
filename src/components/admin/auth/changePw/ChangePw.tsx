d66427b5d4753e7f8ffe8029d8e854c6b01fea4f
LeeJinWoo <wlsdn5793@gmail.com>
2022-06-09 22:30:44 +0900

    ⚡️ :: change-password connect api

--- src/components/admin/auth/changePw/ChangePw.tsx
+++ src/components/admin/auth/changePw/ChangePw.tsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { changepwRequest } from "../../../../lib/api/changePw";
 import * as S from "../../styles";
 const ChangePw = () => {
+  const [inputs, setInputs] = useState({
+    id: "",
+    reg_num: "",
+    pw: "",
+  });
+  const { id, reg_num, pw } = inputs;
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const requestChangepwApi = () => {
+    changepwRequest(id, reg_num, pw)
+      .then(() => {
+        alert("비밀번호 변경이 완료되었습니다.");
+        window.location.href = "/main";
+      })
+      .catch(() => alert("아이디 또는 사업자 등록번호가 일치하지 않습니다."));
+  };
   return (
     <S.Container>
       <S.Title>비밀번호 변경</S.Title>
-      <S.Input placeholder="아이디" />
-      <S.Input placeholder="사업자 등록번호" />
-      <S.Input placeholder="변경할 비밀번호" type="password" />
-      <S.Input placeholder="비밀번호 확인" type="password" />
-      <S.Submit>변경</S.Submit>
+      <S.Input placeholder="아이디" value={id} name="id" onChange={onChange} />
+      <S.Input
+        placeholder="사업자 등록번호"
+        value={reg_num}
+        name="reg_num"
+        onChange={onChange}
+      />
+      <S.Input
+        placeholder="변경할 비밀번호"
+        type="password"
+        value={pw}
+        name="pw"
+        onChange={onChange}
+      />
+      <S.Submit onClick={requestChangepwApi}>변경</S.Submit>
     </S.Container>
   );
 };
