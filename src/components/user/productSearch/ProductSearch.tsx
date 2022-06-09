66bc0f2eff37ebdf5ff3f41d44be7447b3951484
김예진 <wlsdong0004@gmail.com>
2022-06-09 22:11:59 +0900

    ⚡️ :: Product Shearch

--- src/components/user/productSearch/ProductSearch.tsx
+++ src/components/user/productSearch/ProductSearch.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import * as S from "./styles";
 import Header from "../../header/Header";
 import Products from "../../products/Products";
+import ShopInfo from "../../shopInfo/ShopInfo";
 
 const ProductSearch: FC = (): JSX.Element => {
   const items = [
@@ -26,14 +27,188 @@ const ProductSearch: FC = (): JSX.Element => {
       item_name: "item_name",
       style_code: "style_code",
     },
+    {
+      id: 2,
+      image_url: "image_url",
+      brand_name: "brand_name",
+      item_name: "item_name",
+      style_code: "style_code",
+    },
+    {
+      id: 5,
+      image_url: "image_url",
+      brand_name: "brand_name",
+      item_name: "item_name",
+      style_code: "style_code",
+    },
+    {
+      id: 8,
+      image_url: "image_url",
+      brand_name: "brand_name",
+      item_name: "item_name",
+      style_code: "style_code",
+    },
+    {
+      id: 2,
+      image_url: "image_url",
+      brand_name: "brand_name",
+      item_name: "item_name",
+      style_code: "style_code",
+    },
+    {
+      id: 5,
+      image_url: "image_url",
+      brand_name: "brand_name",
+      item_name: "item_name",
+      style_code: "style_code",
+    },
+    {
+      id: 2,
+      image_url: "image_url",
+      brand_name: "brand_name",
+      item_name: "item_name",
+      style_code: "style_code",
+    },
+    {
+      id: 5,
+      image_url: "image_url",
+      brand_name: "brand_name",
+      item_name: "item_name",
+      style_code: "style_code",
+    },
+    {
+      id: 8,
+      image_url: "image_url",
+      brand_name: "brand_name",
+      item_name: "item_name",
+      style_code: "style_code",
+    },
+  ];
+
+  const shops = [
+    {
+      id: 119,
+      name: "구미 편집샵",
+      image_url: "url",
+      opening_hours: "9:00 - 21:00",
+      address: "구미",
+      inventories: [
+        {
+          size: "free",
+          amount: 5,
+        },
+        {
+          size: "free",
+          amount: 5,
+        },
+        {
+          size: "free",
+          amount: 5,
+        },
+        {
+          size: "free",
+          amount: 5,
+        },
+        {
+          size: "free",
+          amount: 5,
+        },
+        {
+          size: "free",
+          amount: 5,
+        },
+        {
+          size: "free",
+          amount: 5,
+        },
+        {
+          size: "free",
+          amount: 5,
+        },
+        {
+          size: "free",
+          amount: 5,
+        },
+        {
+          size: "free",
+          amount: 5,
+        },
+        {
+          size: "free",
+          amount: 5,
+        },
+        {
+          size: "free",
+          amount: 5,
+        },
+        {
+          size: "free",
+          amount: 5,
+        },
+        {
+          size: "free",
+          amount: 5,
+        },
+        {
+          size: "free",
+          amount: 5,
+        },
+      ],
+    },
+    {
+      id: 120,
+      name: "대전 편집샵",
+      image_url: "url",
+      opening_hours: "9:00 - 21:00",
+      address: "대전",
+      inventories: [
+        {
+          size: "free",
+          amount: 5,
+        },
+      ],
+    },
+    {
+      id: 121,
+      name: "용인 편집샵",
+      image_url: "url",
+      opening_hours: "9:00 - 21:00",
+      address: "용인",
+      inventories: [
+        {
+          size: "free",
+          amount: 5,
+        },
+      ],
+    },
+    {
+      id: 122,
+      name: "예산 편집샵",
+      image_url: "url",
+      opening_hours: "9:00 - 21:00",
+      address: "예산",
+      inventories: [
+        {
+          size: "free",
+          amount: 5,
+        },
+      ],
+    },
   ];
   return (
     <div>
-      <Header></Header>
+      <Header />
       <S.Container>
-        {items.map((item, index) => {
-          return <Products key={index} product={item} isSearch={true} />;
-        })}
+        <S.ProductsContainer>
+          {items.map((item, index) => {
+            return <Products key={index} product={item} isSearch={true} />;
+          })}
+        </S.ProductsContainer>
+        <S.ShopsContainer>
+          {shops.map((shop, index) => (
+            <ShopInfo key={index} shopInfo={shop} isMap={false} />
+          ))}
+        </S.ShopsContainer>
       </S.Container>
     </div>
   );
--- src/components/user/productSearch/styles.ts
+++ src/components/user/productSearch/styles.ts
@@ -1,12 +1,40 @@
 import styled from "@emotion/styled";
+import { Lightgray } from "../../../style/color";
 
 export const Container = styled.div`
-  width: calc(100% - 480px);
-  margin: auto;
-  flex-direction: column;
-  padding: 108px 0 0 0;
+  margin: 108px auto 0;
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+  min-width: 1052px;
+`;
+
+export const ProductsContainer = styled.div`
+  padding-bottom: 108px;
+  width: 100%;
+  height: calc(100vh - 208px);
   display: grid;
+  flex-direction: column;
   gap: 48px 36px;
   grid-template-columns: repeat(auto-fill, 244px);
   justify-content: space-evenly;
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const ShopsContainer = styled.div`
+  padding-bottom: 108px;
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 208px);
+  overflow-y: scroll;
+  gap: 36px;
+
+  border-left: solid 1px ${Lightgray};
+  padding-left: 54px;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
