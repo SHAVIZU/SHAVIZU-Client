14d451b2df3ff4a514a3c9078950d89577aedc32
LeeJinWoo <wlsdn5793@gmail.com>
2022-06-13 08:56:42 +0900

    ⚡️ :: change-info 기능 구현

--- src/components/admin/auth/changeInfo/ChangeInfo.tsx
+++ src/components/admin/auth/changeInfo/ChangeInfo.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import * as S from "../../styles";
 const ChangeInfo = () => {
+  const getLocation = (address: string) => {
+    naver.maps.Service.geocode(
+      {
+        query: address,
+      },
+      function (status, response) {
+        if (status !== naver.maps.Service.Status.OK) {
+          return alert("Something wrong!");
+        }
+
+        var result = response.result, // 검색 결과의 컨테이너
+          items = result.items; // 검색 결과의 배열
+        console.log(items);
+        // do Something
+      }
+    );
+  };
+
   return (
     <S.Container>
       <S.Title>편집샵 정보 수정</S.Title>
-      <S.Input placeholder="편집샵 주소" />
-      <S.Input placeholder="편집샵 상세주소" />
-      <S.Input placeholder="편집샵 전화번호" />
-      <S.Input placeholder="편집샵 상세설명" />
-      <S.Map />
-      <S.Submit>변경</S.Submit>
+      <S.Input placeholder="주소" />
+      <S.Input placeholder="상세주소" />
+      <S.Input placeholder="전화번호" />
+      <S.Input placeholder="상세설명" />
+      <S.Input placeholder="영업시간" />
+      <S.Submit onClick={() => getLocation("가정북로76")}>변경</S.Submit>
     </S.Container>
   );
 };
--- src/pages/auth/changeInfo/ChangeInfoPage.tsx
+++ src/pages/auth/changeInfo/ChangeInfoPage.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from "react";
 import ChangeInfo from "../../../components/admin/auth/changeInfo/ChangeInfo";
-
+import Header from "../../../components/admin/header/Header";
 const ChangeInfoPage: FC = (): JSX.Element => {
   return (
     <>
+      <Header />
       <ChangeInfo />
     </>
   );
